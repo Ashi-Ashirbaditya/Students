ccc2ac44d8ff9c6188ba54298eb0b4f6ba5343a2
Ashi-Ashirbaditya <ashirbaditya@gmail.com>
2025-11-17 17:00:05 +0530

    html error page

--- src/main/resources/static/css/js/downl.js
+++ src/main/resources/static/css/js/downl.js
@@ -2,7 +2,21 @@ function downloadExcel() {
     let table = document.getElementById("myTable");
     let html = table.outerHTML.replace(/ /g, '%20');
 
-    let fileName = "table_data.xls";
+    let fileName = "student_data.xls";
+
+    let downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    downloadLink.href = 'data:application/vnd.ms-excel,' + html;
+    downloadLink.download = fileName;
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+function downloadExcel2() {
+    let table = document.getElementById("myTable");
+    let html = table.outerHTML.replace(/ /g, '%20');
+
+    let fileName = "course_data.xls";
 
     let downloadLink = document.createElement("a");
     document.body.appendChild(downloadLink);
